cd641e3e67302316d66b5d947421d4fdf865e997
paloma.calado <paloma.calado@ambevtech.com.br>
2021-08-18 18:09:07 -0300

    javascript - two-fer

---
+++ teams/start-tech/javascript/two-fer/two-fer.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (nome) => {
+  
+  
+  if( nome != undefined){
+    return 'One for '+ nome + ', one for me.';
+  }
+  
+  else{
+    return 'One for you, one for me.';
+  }
+
+
+
+};

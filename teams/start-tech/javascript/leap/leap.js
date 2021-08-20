af8eccff273868e95854c3436244c04009f81ecf
paloma.calado <paloma.calado@ambevtech.com.br>
2021-08-20 09:36:47 -0300

    resolução do problema

--- teams/start-tech/javascript/leap/leap.js
+++ teams/start-tech/javascript/leap/leap.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const isLeap = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isLeap = (ano) => {
+
+  
+  const divisivelPor4 = ano%4 === 0; // verifica se o ano é divisível por 4
+  const divisivelPor100 = ano%100 === 0; // verifica se o ano é divisível por 100
+  const divisivelPor400 = ano%400 === 0; // verifica se o ano é divisível por 400
+  const testaBissexto = divisivelPor4 && !divisivelPor100 || divisivelPor400; // regras para ser bissexto
+  
+  //retorna se o ano é bissexto ou não 
+  return testaBissexto ;
+  
 };

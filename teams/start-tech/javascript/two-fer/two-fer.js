2fb839465b92fe40740a06f4a1cc2e7ac72cd833
paloma.calado <paloma.calado@ambevtech.com.br>
2021-08-24 08:33:46 -0300

    formas diferentes de retornar string com variável

--- teams/start-tech/javascript/two-fer/two-fer.js
+++ teams/start-tech/javascript/two-fer/two-fer.js
@@ -7,7 +7,8 @@ export const twoFer = (nome) => {
   
   //testa se a entrada é indefinida e diferente de vazia
   if( nome != undefined && nome != '' ){
-    return 'One for '+ nome + ', one for me.';
+    return 'One for '+ nome + ', one for me.'; // a soma concatena as strings
+                                               // uma outra forma é 'One for ${nome}$, one for me'
   }
   // testa se a entrada é nula ou indefinida ou vazia
   if (nome == null || nome == undefined || nome == '' ){

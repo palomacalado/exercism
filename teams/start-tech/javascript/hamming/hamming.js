a46116dec77742d4574b929a38f79fe33896ba6f
paloma.calado <paloma.calado@ambevtech.com.br>
2021-08-25 09:49:51 -0300

    resolução da comparação de duas sequencias de dna e aponta o número de diferenças entre elas

---
+++ teams/start-tech/javascript/hamming/hamming.js
@@ -0,0 +1,34 @@
+//
+// This is only a SKELETON file for the 'diferencas
+' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const compute = (direita, esquerda) => {
+  
+  let diferencas = 0;
+  //testa se a direita ou a esquerda são vazios
+  if(esquerda === direita){
+    return 0;
+  //testa se o da esquerda é vazio
+  }else if(esquerda === ''){
+    throw new Error('right strand must not be empty');
+  //testa se direita é vazio
+  }else if(direita === ''){
+    throw new Error('left strand must not be empty');
+
+  //testa se o comprimento dos dois são iguais
+  }else if(esquerda.length !== direita.length){
+
+    throw new Error('left and right strands must be of equal length');
+  
+  }else{
+    // conta o numero de diferenças entre as strings
+    for (let i = 0; i < esquerda.length; i++) {
+      if(esquerda[i]!== direita[i]){
+        diferencas++;
+      }
+    }
+    return diferencas;
+  }
+};

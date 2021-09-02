2d7888e4785fb4d74b0851f1960aa68d1faa5fb6
paloma.calado <paloma.calado@ambevtech.com.br>
2021-09-02 16:49:59 -0300

    resolução

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,29 @@
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+export const steps = (numero) => {
+  
+  let contador = 0;
+  let numeroAux = numero;
+
+  if(numero <= 0){
+    throw new Error('Only positive numbers are allowed')
+  } 
+  
+
+  while(numeroAux !== 1){
+    if (numeroAux %2 === 0) {
+      numeroAux = numeroAux/2;
+    }else{
+      numeroAux = 3*numeroAux +1;
+    }
+    contador ++;
+  }
+
+  return contador;
+  
+    
+}

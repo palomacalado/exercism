bf32a114083772911bf84de14d5dfa05f784caa1
paloma.calado <paloma.calado@ambevtech.com.br>
2021-09-08 13:56:33 -0300

    resolução

---
+++ teams/start-tech/javascript/bank-account/bank-account/bank-account.js
@@ -0,0 +1,32 @@
+export class BankAccount {
+    constructor() {
+        
+    }
+
+    open() {
+       
+
+    }
+
+    close() {
+       
+    }
+
+    deposit(money) {
+        
+    }
+
+    withdraw(money) {
+       
+    }
+
+    get balance() {
+       ;
+    }
+}
+
+export class ValueError extends Error {
+    constructor() {
+        super('Bank account error');
+    }
+}

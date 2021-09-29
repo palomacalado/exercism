8d458954e3c360f80063443d8dc198ddaf53ec26
paloma.calado <paloma.calado@ambevtech.com.br>
2021-09-29 17:21:15 -0300

    resolução two-fer

--- typescript/two-fer/two-fer.ts
+++ typescript/two-fer/two-fer.ts
@@ -2,7 +2,7 @@
  * This stub is provided to make it straightforward to get started.
  */
 
-export function twoFer(): string {
+export function twoFer(nome?:string ): string {
   // ^                 ^   ^ this is called a return type; it's the type of the
   // ^                 ^     value that is returned from this function
   // ^                 ^
@@ -10,4 +10,14 @@ export function twoFer(): string {
   // ^
   // allows the tests to import this function and call it
   // <-- Your code goes here. You may remove all the commentary in this file.
+  if (nome == undefined){
+    nome = "you";
+  }
+  switch(nome){
+    case undefined:
+      return `One for you, one for me.`;
+    default:
+      return `One for ${nome}, one for me.`;
+  }
+
 }

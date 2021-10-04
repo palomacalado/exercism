ddcd729c13a557e29ce2d65d4317d038dcf1be58
paloma.calado <paloma.calado@ambevtech.com.br>
2021-10-04 12:44:27 -0300

    resolução pangram

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,3 +1,10 @@
-export function isPangram() {
-  throw new Error('Remove this statement and implement this function')
+
+export function isPangram(frase: string):boolean {
+
+  let fraseFiltrada = frase.toUpperCase().split("").filter(caracter => caracter >= 'A' && caracter <= 'Z');
+
+  const setFraseFiltrada = new Set(fraseFiltrada); // o set ignora todos os caracteres repitidos
+
+  return setFraseFiltrada.size == 26;
+  
 }

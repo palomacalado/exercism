a9c291144f7d1c39c2f01cd6d1e88559e8b6c9e3
paloma.calado <paloma.calado@ambevtech.com.br>
2021-10-04 11:34:46 -0300

    resolução space age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,19 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+type Planet = keyof typeof idadePlanetas;
+enum idadePlanetas {
+
+  earth =  31557600,
+  mercury = 0.2408467 * earth,
+  venus = 0.61519726 * earth,
+  mars = 1.8808158 * earth,
+  jupiter = 11.862615 * earth,
+  saturn = 29.447498 * earth,
+  uranus = 84.016846 * earth,
+  neptune = 164.79132  * earth
+
+};
+
+
+export function age(planet: Planet, seconds: number): number {
+
+  return  + (seconds/ +idadePlanetas[planet]).toFixed(2) ;
 }

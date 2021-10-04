a3aabd4d617e34553ac943fd9cb2e957a2e5d165
paloma.calado <paloma.calado@ambevtech.com.br>
2021-10-04 12:26:56 -0300

    resolução rna

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,27 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+import { TypeofTypeAnnotation } from "@babel/types";
+
+type baseDna = keyof typeof basesDna;
+
+enum basesDna{
+  G = 'C',
+  C = 'G',
+  T = 'A',
+  A = 'U'
 }
+
+export function toRna(dna: string): string {
+let resultado :string = "";
+ 
+for (const elemento of dna){
+  if (!(elemento in basesDna)) {
+    throw new Error ("Invalid input DNA.");
+  }
+
+  resultado += basesDna[<baseDna>elemento];
+
+}
+
+return resultado;
+}
+
+
